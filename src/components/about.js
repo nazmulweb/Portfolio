f11ff7999e08ee9bfd8bcf76d8d70fe0de021261
Nazmul Hasan <nazmulhasan598040@gmail.com>
2020-03-18 10:07:01 +0600

    Working on about me section

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import Photo from '../assets/img/me.JPG'
+import { Title } from './common'
 
 export class About extends Component {
     render() {
         return (
             <React.Fragment>
-                
+                <section className="about-area mt-5">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <Title title="About Me" mtBottom = "40px" />
+                            </div>
+                            <div className="col-sm-12 col-md-6">
+                                <div className="about-left">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa vero, earum rerum mollitia quis deserunt, perferendis velit debitis quasi placeat, dolor sequi? Assumenda rem provident sit vel quasi saepe, eveniet corporis minus recusandae explicabo iusto similique exercitationem? Consequuntur hic quaerat corrupti, veritatis sit beatae quibusdam fugit consequatur veniam expedita!</p>
+                                    <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, perspiciatis. Totam eligendi voluptatem fugiat modi quidem nobis assumenda obcaecati praesentium?</p>
+                                </div>
+                            </div>
+                            <div className="col-sm-12 col-md-6">
+                                <img src={Photo} alt="profile"/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </React.Fragment>
         )
     }
--- src/components/common/title.js
+++ src/components/common/title.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './title.css'
 
-const Title = ({title})  => {
+const Title = ({title, mtBottom})  => {
     return (
         <React.Fragment>
-            <h4 className="title">{title}</h4>
+            <h4 className="title" style={{marginBottom: mtBottom ? mtBottom : ''}}>{title}</h4>
         </React.Fragment>
     )
 }
